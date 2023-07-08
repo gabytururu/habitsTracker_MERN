745340f450c870bf42d00c7d9c09d318e2eb54bd
gabriela tururu <gabriela.aguayaire@gmail.com>
2023-07-07 19:06:22 -0600

    WIP testing frontend

--- frontend/src/components/HabitsList.js
+++ frontend/src/components/HabitsList.js
@@ -1,15 +1,17 @@
-// import {useState} from 'react'
+import { Link } from 'react-router-dom'
 const HabitsList = ({habit}) => {
     
     //const {title, reps} = habits
     //const [habits,setHabits] = useState(habits)
     return ( 
-       
-            <div className="habitCard">
-                <p>habit name:{habit.title}</p>
-                <p>habit reps:{habit.reps}</p>           
-                <p>Habit id:{habit.id}</p>          
-            </div>
+
+            <Link to='/reports'>
+                <div className="habitCard">
+                    <p>habit name:{habit.title}</p>
+                    <p>habit reps:{habit.reps}</p>           
+                    <p>Habit id:{habit.id}</p>          
+                </div>
+            </Link>
            
        
      );
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -8,8 +8,8 @@ const Home = () => {
             const fetchData = async() =>{
 
                 try{
-                const fetchedObject = await fetch('http://localhost:4000/api/habits')
-                const fetchedJsonData = await fetchedObject.json()
+                    const fetchedObject = await fetch('http://localhost:4000/api/habits')
+                    const fetchedJsonData = await fetchedObject.json()
                 
                 if(fetchedObject.ok){
                     console.log(fetchedObject)
@@ -20,36 +20,9 @@ const Home = () => {
                 }catch(err){
                     console.log(err)
                 }
-
-                            
+              
             }
             fetchData()
-            
-            
-            // const habitsList = [
-            //     {
-            //         title:'one habit', 
-            //         trackingMethod: '3x per week', 
-            //         reps:'50',
-            //         id:1
-            //     }, 
-            //     {
-            //         title:'two habit',
-            //         trackingMethod: '3x per week', reps:'50',
-            //         id:2
-            //     }, 
-            //     {
-            //         title:'three habit', trackingMethod: '3x per week', reps:'50',
-            //         id:3
-            //     }, 
-            //     {
-            //         title:'...n habit',
-            //         trackingMethod: '3x per week', reps:'50',
-            //         id:4
-            //     }]      
-           
-            // setHabits(habitsList)
-            // console.log(habits)
         },[])
        
         
